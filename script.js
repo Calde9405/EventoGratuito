120b4a315b4f2766b3d6346294192e97e16f81d0
Calde9405 <132153838+Calde9405@users.noreply.github.com>
2025-01-02 23:09:34 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+
+// Mostrar el popup
+function openModal() {
+const popup = document.getElementById("popup");
+popup.style.display = "flex"; // Cambiar a 'block' o el estilo que prefieras
+}
+
+
+// Función que redirige al grupo de WhatsApp
+function redirectToWhatsApp() {
+    // Aquí va el enlace al grupo de WhatsApp
+    window.location.href = "https://chat.whatsapp.com/JYTqi0DETtL2oBxFKZXjBY";  // Reemplaza con tu enlace de WhatsApp
+}
